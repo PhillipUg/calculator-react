ca47215ecb58134eeb5aee5918ff8bae2ed1d432
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-21 20:32:13 +0300

    add components folder and add button, buttonPanel, display and app components

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const ButtonPanel = () => {
+  return (
+    <div>
+      <div>
+        <button>AC</button>
+        <button>+/-</button>
+        <button>&#37;</button>
+        <button>&#247;</button>
+      </div>
+      <div>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>&#215;</button>
+      </div>
+      <div>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>&#8722;</button>
+      </div>
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>&#43;</button>
+      </div>
+      <div>
+        <button>0</button>
+        <button>&#8729;</button>
+        <button>&#61;</button>
+      </div>
+    </div>
+  )
+}
+export default ButtonPanel;
