803b74549b35061844873744dcc1f2f231ad1f49
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-22 23:23:59 +0300

    add handleClick func and pass it to buttonPanel as prop. pass result to display

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate'; /*eslint-disable-line*/
+import calculate from '../logic/calculate';
+// import calculate from '../logic/calculate';
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       total: null,
       next: null,
-      operation: null
-    }
+      operation: null /*eslint-disable-line*/
+    };
   }
 
-  handleClick = (btnName) => {
-    const result = calculate(this.state.total, this.state.next, btnName)
+  handleClick(btnName) {
+    this.setState(data => calculate(data, btnName));
   }
 
   render() {
+    const { total, next } = this.state;
+    const result = next || total || '0';
     return (
       <div id="root-app">
-        <Display />
-        <ButtonPanel />
+        <Display result={result.toString()} />
+        <ButtonPanel handleClick={this.handleClick} />
       </div>
     );
   }
