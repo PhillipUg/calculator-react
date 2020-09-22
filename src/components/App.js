9f0abf517074d8aa36ae11d1ff57890bd6459506
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-22 19:59:05 +0300

    changed App functional component to class

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,28 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate'; /*eslint-disable-line*/
 
-function App() {
-  return (
-    <div id="root-app">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    }
+  }
+
+  handleClick = (btnName) => {
+    const result = calculate(this.state.total, this.state.next, btnName)
+  }
+
+  render() {
+    return (
+      <div id="root-app">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
