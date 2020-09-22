291041339e466b8f3b1774534f7fb74674697fe0
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-22 08:48:37 +0300

    correct linter errors

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({ result }) => {
-  return (
-    <div>
-      {result}
-    </div>
-  )
-}
+const Display = ({ result }) => (
+  <div>
+    {result}
+  </div>
+);
 
 Display.propTypes = {
-  result: PropTypes.string
-}
+  result: PropTypes.string,
+};
 
 Display.defaultProps = {
-  result: "0"
-}
+  result: '0',
+};
 
 export default Display;
