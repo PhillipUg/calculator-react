e2030a29ed34d5dfc5626fabf9bfa6c238272dab
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-22 16:18:37 +0300

    correct linter errors

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,7 +1,7 @@
 import operate from './operate';
 
-const calculate = (calculator, btn) => {
-  let { total, next, operation } = calculator;
+const calculate = (data, btn) => {
+  let { total, next, operation } = data;
   const operators = ['+', '&times;', '&minus;', '÷'];
   const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
