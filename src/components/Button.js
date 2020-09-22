8e4e43c1074c7e2349da5521cc14dfbf6557097f
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-22 07:37:56 +0300

    btn accepts name prop. prop should be string

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ name }) => {
+  return (
+    <div>
+      {name}
+    </div>
+  )
+}
+
+Button.propTypes = {
+  name: PropTypes.string
+}
+
+export default Button;
